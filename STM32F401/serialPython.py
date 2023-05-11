129415f608fe09fbb56424b917b6c36a4a9a85e6
Jormunth <jormunth@gmail.com>
2023-05-11 09:33:07 +0200

    dv

--- STM32F401/serialPython.py
+++ STM32F401/serialPython.py
@@ -1,5 +1,6 @@
 # pip install pyserial
 from serial import Serial
+import keyboard
 
 ser = Serial()
 ser.baudrate = 19200 # standard Baudrate for STM32F4 devices
@@ -10,6 +11,7 @@ ser.stopbits=1
 ser.timeout=None
 ser.xonxoff=0
 ser.rtscts=0
+ser.timeout=1
 
 # x = ser.read()          # read one byte
 # s = ser.read(10)        # read up to 10 bytes (timeout)
@@ -18,5 +20,21 @@ ser.rtscts=0
 if ser.open():
     print(ser,"\n")
     while True:
+        if keyboard.is_pressed("a"):
+            print("stop")
+            ser.write(b"stop>")
+        elif keyboard.is_pressed("z"):
+            print("forward")
+            ser.write(b"move:forward")
+        elif keyboard.is_pressed("s"):
+            print("backward")
+            ser.write(b"move: backward")
+        elif keyboard.is_pressed("q"):
+            print("left")
+            ser.write(b"turn: left")
+        elif keyboard.is_pressed("d"):
+            print("right")
+            ser.write(b"turn: right")
+
         line = str(ser.readline()) 
-        print(line,"\n")
+        print(line,"\n") 
